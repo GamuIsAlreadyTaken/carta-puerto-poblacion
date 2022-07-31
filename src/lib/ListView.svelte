b13bb730e328669e5552cf2f85453292329cf313
GamuCEEP <GamuDiez@gmail.com>
2022-07-31 19:22:58 +0200

    Some touches

--- src/lib/ListView.svelte
+++ src/lib/ListView.svelte
@@ -3,7 +3,7 @@
 
   export let list;
   export let _importance = 2;
-  const element = list.price ? `p` : `h${_importance}`;
+  const element = list.price ? `span` : `h${_importance}`;
   const cssclass = `title${_importance}`;
 
   const formatter = Intl.NumberFormat($language, {
@@ -15,13 +15,14 @@
 <div class={cssclass}>
   <svelte:element this={element}>{list[$language]}</svelte:element>
   {#if list.price}
-    <svelte:element this={element}>
+    <svelte:element this={element} class="price">
       {typeof list.price == 'number'
         ? formatter.format(list.price)
         : list.price}
     </svelte:element>
   {/if}
 </div>
+
 {#if list.items}
   {#each list.items as item}
     <svelte:self list={item} _importance={_importance + 1} />
@@ -34,15 +35,25 @@
     flex-direction: row;
     justify-content: space-between;
   }
-  .title2 {
-    justify-content: center;
+  .title2 *{
+    text-align: center;
+    width: 100%;
   }
-  .title3 {
+  .title3 *{
     text-decoration: underline;
     margin-top: 20px;
-    margin-bottom: 0;
+    margin-bottom: 10px;
+  }
+  .title4{
+    border-bottom: 1px solid #fff4;
   }
   .title4 * {
-    margin: 0;
+    margin: 0 0 0 10px;
+    justify-self: center;
+  }
+  .price{
+    border: none;
+    position: relative;
+    bottom: 0;
   }
 </style>
